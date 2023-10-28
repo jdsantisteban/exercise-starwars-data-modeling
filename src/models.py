16766736174695acc1f86a7ff8eace4cef39cba8
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-10-28 19:10:57 +0000

    Added relationships to the DB

--- src/models.py
+++ src/models.py
@@ -46,53 +46,5 @@ class Planet(Base):
 
 
 
-
-# # one to one
-# class Parent(Base):
-#     __tablename__='parents'
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String(80), nullable=False)
-#     child = relationship('Child',  uselist=False)
-
-# class Child(Base):
-#     __tablename__='children'
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String(80), nullable=False)
-#     parent_id = Column(Integer(), ForeignKey('parents.id'))
-
-# # one to many
-# class User(Base):
-#     __tablename__='user'
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String(40), nullable=False, unique=True)
-#     email = Column(String(80), nullable=False, unique=True)
-#     posts = relationship('Post', uselist=True, backref='user')
-
-# class Post(Base):
-#     __tablename__='posts'
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String(255), nullable=False)
-#     content = Column(Text, nullable=False)
-#     user_id = Column(Integer, ForeignKey('user.id'))
-
-# # many to many
-# class Customer(Base):
-#     __tablename__='customer'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(80), nullable=False)
-#     product_customer = relationship('Product_Customer', uselist=True, backref='customer')
-
-# class Product(Base):
-#     __tablename__='product'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(80), nullable=False)
-#     product_customer = relationship('Product_Customer', uselist=True, backref='product')
-
-# class Product_Customer(Base):
-#     __tablename__='product_customer'
-#     id = Column(Integer, primary_key=True)
-#     customer_id = Column(Integer, ForeignKey('customer.id'))
-#     product_id = Column(Integer, ForeignKey('product.id'))
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
